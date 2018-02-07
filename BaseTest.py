65d213fceaf2eedb9d6da44d586788c4bfe674c2
pavka <pavel-mosein@yandex.ru>
2018-02-07 23:21:06 +0300

    fixes

--- BaseTest.py
+++ BaseTest.py
@@ -2,9 +2,9 @@ import unittest
 from BookBase import mainFunc 
 
 class Testing(unittest.TestCase):
-	def isIn(self):
-		self.assertIn('Война', mainFunc('Наступило молчание. Графиня глядела на гостью, приятно улыбаясь, впрочем, не скрывая того')[0])
-		self.assertIn('Толстой', mainFunc('Наступило молчание. Графиня глядела на гостью, приятно улыбаясь, впрочем, не скрывая того')[1])
+    def isIn(self):
+        self.assertIn('Война', mainFunc('Наступило молчание. Графиня глядела на гостью, приятно улыбаясь, впрочем, не скрывая того')[0])
+        self.assertIn('Толстой', mainFunc('Наступило молчание. Графиня глядела на гостью, приятно улыбаясь, впрочем, не скрывая того')[1])
 
 if __name__ == '__main__':
     unittest.main()
--- BookBase.py
+++ BookBase.py
@@ -7,34 +7,32 @@ from bs4 import BeautifulSoup
 ##raw = "Святой стал смеяться над Заратустрой и так говорил: «Тогда постарайся, чтобы они приня-ли твои сокровища! Они недоверчивы к отшельникам и не верят, что мы приходим, чтобы да-рить."
 
 def mainFunc(raw):
-	raw = raw.split(' ')
-	currentstring = ''
-	newraw = []
-	for i in raw:
-		for j in i:
-			if j == ',':
-				currentstring += '%2C'
-			else:
-				currentstring += str(j)
-		newraw.append(currentstring)
-		currentstring = ''
-	newraw = "+".join(newraw)
-	link = "http://www.google.ru/search?tbm=bks&hl=ru&q="+newraw+"&num=1"
-	response = requests.get(link)
-	soup = BeautifulSoup(response.text, 'html.parser')
-	bookname = str(soup.find_all("h3", class_="r")).split('">')[2].split('</a>')[0].split('&amp;')[0]
-	print('Скорее всего это: ', bookname)
-	author = str(soup.find_all("span", class_="f")).split('a class="fl"')[1].split('>')[1].split('<')[0]
-	print('Автора: ', author)
-	print(" ")
-	
-	return([bookname, author])
+    raw = raw.split(' ')
+    currentstring = ''
+    newraw = []
+    for i in raw:
+        for j in i:
+            if j == ',':
+                currentstring += '%2C'
+            else:
+                currentstring += str(j)
+        newraw.append(currentstring)
+        currentstring = ''
+    newraw = "+".join(newraw)
+    link = "http://www.google.ru/search?tbm=bks&hl=ru&q="+newraw+"&num=1"
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    bookname = str(soup.find_all("h3", class_="r")).split('">')[2].split('</a>')[0].split('&amp;')[0]
+    author = str(soup.find_all("span", class_="f")).split('a class="fl"')[1].split('>')[1].split('<')[0]
+
+    return bookname, author
 
 
 if __name__ == "__main__":
-	while True:
-		raw = input("Ну что-с, давайте сюда вашу цитатку: ")
-		raw = "Наступило молчание. Графиня глядела на гостью, приятно улыбаясь, впрочем, не скрывая того,"	
-		returned = mainFunc(raw)
-		author = returned[1]
-		book = returned[0]
+    while True:
+        raw = input("Ну что-с, давайте сюда вашу цитатку: ")
+        raw = "Наступило молчание. Графиня глядела на гостью, приятно улыбаясь, впрочем, не скрывая того,"
+        book, author = mainFunc(raw)
+        print('Скорее всего это: ', bookname)
+        print('Автора: ', author)
+        print(" ")
